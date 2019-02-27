65aaa6df686772427ca5d35720c17aef847f383d
montoyedse <43078469+montoyedse@users.noreply.github.com>
2019-02-26 19:26:29 -0600

    Fixed the debugger bug
    Fixed the bug where the speed showed 0 after disabling the debug

--- src/synergy_gen/sensors_thread.h
+++ src/synergy_gen/sensors_thread.h
@@ -9,15 +9,20 @@ extern "C" void sensors_thread_entry(void);
 #else 
 extern void sensors_thread_entry(void);
 #endif
-#include "r_icu.h"
-#include "r_external_irq_api.h"
 #include "r_gpt.h"
 #include "r_timer_api.h"
+#include "r_icu.h"
+#include "r_external_irq_api.h"
 #include "r_adc.h"
 #include "r_adc_api.h"
 #ifdef __cplusplus
 extern "C"
 {
+#endif
+/** Timer on GPT Instance. */
+extern const timer_instance_t g_timer0;
+#ifndef g_timer0_interrupt
+void g_timer0_interrupt(timer_callback_args_t *p_args);
 #endif
 /* External IRQ on ICU Instance. */
 extern const external_irq_instance_t g_external_irq5;
